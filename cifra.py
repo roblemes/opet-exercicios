7873bd460af526ee98441c82c4869e00cd02e8c8
Robson <robsonlemesz@gmail.com>
2020-08-16 16:56:23 -0300

    alteracao

--- cifra.py
+++ cifra.py
@@ -1,15 +1,32 @@
 import sys
 
+ALFA='abcdefghijklmnopqrstuvwxyz'
+ROT=3
+
+def cifra(mensagem, dir):
+    m=''
+    for x in mensagem:
+        if x in ALFA:
+            x_index = ALFA.index(x)
+            m += ALFA[(x_index + (dir * ROT)) % len(ALFA)]
+        else:
+            m += x
+    return m
+
+def decifrar(mensagem):
+    return cifra(mensagem, -1)
+def cifrar(mensagem):
+    return cifra(mensagem, 1)
+
 def main():
-    command = sys.argv[1].lower()
-    message = sys.argv[2].lower()
+    comando = sys.argv[1].lower()
+    mensagem = sys.argv[2].lower()
 
-    if command == 'decifrar':
-        print ('decifrar')
-    elif command == 'cifrar':
-        print ('cifrar')
+    if comando == 'decifrar':
+        print (decifrar(mensagem))
+    elif comando == 'cifrar':
+        print (cifrar(mensagem))
     else:
-        print (command +' -> comando nao encontrado')
-
+        print (comando +' -> comando nao encontrado')
 
 main()
